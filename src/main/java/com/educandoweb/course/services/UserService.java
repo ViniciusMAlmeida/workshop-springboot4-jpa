fef267b5bc85f508eb982f8494ac8ea3754e4004
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2026-02-02 17:36:15 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
+	
+	public User save(User obj) {
+		return userRepository.save(obj);
+	}
 }
